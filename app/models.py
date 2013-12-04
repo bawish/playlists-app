bac89a77ac38ce5fce2389c3bdd972bdcbe9d544
bawish <barretts@gmail.com>
2013-12-03 17:40:29 -0800

    fixed some bugs in models.py and updated .gitignore with .db and more

--- app/models.py
+++ app/models.py
@@ -6,8 +6,8 @@ ROLE_ADMIN = 1
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	email = db.Column(db.String(120), index = True, unique = True)
-	oauth_token = db.Column(String(200))
-	oauth_secret = db.Column(String(200))
+	oauth_token = db.Column(db.String(200))
+	oauth_secret = db.Column(db.String(200))
 	
-	df __repr__(self):
+	def __repr__(self):
 		return '<User %r>' % (self.email)
