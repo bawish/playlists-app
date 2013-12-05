210a0219e9725547922eb6c58722b12b662c43de
bawish <barretts@gmail.com>
2013-12-04 20:31:44 -0500

    adds a couple lines to __init__ to import and initiate OAuth()

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_oauth import OAuth # HOPE THIS WORKS
 
 app = Flask(__name__) # initialize app object
 app.config.from_object('config') # link to config file
 db = SQLAlchemy(app) # create db variable
+oauth = Oauth() # HOPE THIS WORKS
 
 from app import views, models
